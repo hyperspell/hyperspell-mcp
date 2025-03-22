bcc875492475410a1250993a72ddf7249b42a872
Manu Ebert <manuel@1450.me>
2025-03-21 17:36:24 -0700

    Get Collections and documents
    Closes F-394

--- hyperspell-mcp/server.py
+++ hyperspell-mcp/server.py
@@ -1,10 +1,11 @@
 # server.py
 import os
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, Self, Sequence, overload
 
 from hyperspell import Hyperspell
 from mcp.server.fastmcp import FastMCP
+from pydantic import BaseModel as PydanticBaseModel
 
 token = os.getenv("HYPERSPELL_TOKEN")
 
@@ -34,3 +35,64 @@ def tool_or_resource(uri: str, name: str | None = None):
 
     return decorator
 
+
+@dataclass
+class BaseModel:
+    @overload
+    @classmethod
+    def from_pydantic(cls, model: PydanticBaseModel) -> Self: ...
+
+    @overload
+    @classmethod
+    def from_pydantic(cls, model: Sequence[PydanticBaseModel]) -> list[Self]: ...
+
+    @classmethod
+    def from_pydantic(
+        cls, model: PydanticBaseModel | Sequence[PydanticBaseModel]
+    ) -> Self | list[Self]:
+        if isinstance(model, Sequence):
+            return [cls.from_pydantic(m) for m in model]
+
+        data = model.model_dump()
+        # Only select the keys in data that are part of this data class
+        data = {key: value for key, value in data.items() if key in cls.__annotations__}
+        return cls(**data)
+
+
+@dataclass
+class Collection(BaseModel):
+    name: str
+    documents_count: int = 0
+
+
+@dataclass
+class Document(BaseModel):
+    id: int
+    source: str
+    resource_id: str
+    title: str | None
+    created_at: str
+    collection: str
+    type: str
+    data: dict | None
+
+
+@tool_or_resource("collection://", name="List Collections")
+def list_collections() -> list[Collection]:
+    """Get a list of all collections on Hyperspell"""
+    r = hyperspell.collections.list()
+    return Collection.from_pydantic(r.items)
+
+
+@tool_or_resource("collection://{collection_name}", name="Get Collection")
+def get_documents(collection_name: str) -> list[Document]:
+    """Get a list of all documents in a collection"""
+    r = hyperspell.documents.list(collection=collection_name)
+    return Document.from_pydantic(r.items)
+
+
+@tool_or_resource("document://{document_id}", name="Get Document")
+def get_document(document_id: int) -> Document:
+    """Get a document from a collection"""
+    r = hyperspell.documents.get(document_id=document_id)
+    return Document.from_pydantic(r)
