ec6fe0af1a8674c2e0ab5a872539ea212625c177
Manu Ebert <manuel@1450.me>
2025-03-26 21:58:38 -0600

    Make __main__ DRY

--- src/hyperspell_mcp/__main__.py
+++ src/hyperspell_mcp/__main__.py
@@ -1,10 +1,3 @@
-from . import server
+from hyperspell_mcp import main
 
-
-def main():
-    """Main entry point for the package."""
-    server.main()
-
-
-# Optionally expose other important items at package level
-__all__ = ["main", "server"]
+main()
